8f6373a1b87d566d21097da086bc90db92b8783e
Preventionsanteenvironnement <pseaclyon@gmail.com>
2026-02-08 07:31:48 +0100

    Add files via upload

---
+++ data_eleves.js
@@ -0,0 +1,205 @@
+/* 📁 DATA_ELEVES.JS — VERSION RGPD
+   
+   ✅ Ce fichier est RGPD compliant
+   🔒 Il contient uniquement : userCode + classe (pas de données personnelles)
+   
+   📅 Mise à jour : 26 janvier 2026
+   👥 Total : 150 élèves
+*/
+
+window.BDD_ELEVES = [
+    // B1AGO1 (8 élèves)
+    { userCode: "KA47", classe: "B1AGO1" },
+    { userCode: "LU83", classe: "B1AGO1" },
+    { userCode: "MO12", classe: "B1AGO1" },
+    { userCode: "QF59", classe: "B1AGO1" },
+    { userCode: "RA26", classe: "B1AGO1" },
+    { userCode: "TI74", classe: "B1AGO1" },
+    { userCode: "NE08", classe: "B1AGO1" },
+    { userCode: "SA91", classe: "B1AGO1" },
+
+    // B1AGO2 (10 élèves)
+    { userCode: "VO35", classe: "B1AGO2" },
+    { userCode: "PY64", classe: "B1AGO2" },
+    { userCode: "XK19", classe: "B1AGO2" },
+    { userCode: "DF82", classe: "B1AGO2" },
+    { userCode: "RT57", classe: "B1AGO2" },
+    { userCode: "ML03", classe: "B1AGO2" },
+    { userCode: "HG68", classe: "B1AGO2" },
+    { userCode: "CP41", classe: "B1AGO2" },
+    { userCode: "AJ90", classe: "B1AGO2" },
+    { userCode: "BN25", classe: "B1AGO2" },
+
+    // B1MELEC (7 élèves)
+    { userCode: "EU14", classe: "B1MELEC" },
+    { userCode: "SO76", classe: "B1MELEC" },
+    { userCode: "KT52", classe: "B1MELEC" },
+    { userCode: "WD88", classe: "B1MELEC" },
+    { userCode: "LP09", classe: "B1MELEC" },
+    { userCode: "YM63", classe: "B1MELEC" },
+    { userCode: "RF31", classe: "B1MELEC" },
+
+    // B2GATL1 (8 élèves)
+    { userCode: "CZ72", classe: "B2GATL1" },
+    { userCode: "QL16", classe: "B2GATL1" },
+    { userCode: "MX48", classe: "B2GATL1" },
+    { userCode: "VA95", classe: "B2GATL1" },
+    { userCode: "HU27", classe: "B2GATL1" },
+    { userCode: "PN60", classe: "B2GATL1" },
+    { userCode: "SJ04", classe: "B2GATL1" },
+    { userCode: "DK81", classe: "B2GATL1" },
+
+    // B2GATL2 (8 élèves)
+    { userCode: "TG36", classe: "B2GATL2" },
+    { userCode: "AR58", classe: "B2GATL2" },
+    { userCode: "ZN73", classe: "B2GATL2" },
+    { userCode: "FK10", classe: "B2GATL2" },
+    { userCode: "WL92", classe: "B2GATL2" },
+    { userCode: "BM45", classe: "B2GATL2" },
+    { userCode: "EO67", classe: "B2GATL2" },
+    { userCode: "JC21", classe: "B2GATL2" },
+
+    // B2MELEC (8 élèves)
+    { userCode: "PS84", classe: "B2MELEC" },
+    { userCode: "NV33", classe: "B2MELEC" },
+    { userCode: "KQ56", classe: "B2MELEC" },
+    { userCode: "LR07", classe: "B2MELEC" },
+    { userCode: "UH69", classe: "B2MELEC" },
+    { userCode: "XT18", classe: "B2MELEC" },
+    { userCode: "DF94", classe: "B2MELEC" },
+    { userCode: "MA02", classe: "B2MELEC" },
+
+    // BTAGO1 (9 élèves)
+    { userCode: "RL61", classe: "BTAGO1" },
+    { userCode: "ZC28", classe: "BTAGO1" },
+    { userCode: "PW75", classe: "BTAGO1" },
+    { userCode: "SF13", classe: "BTAGO1" },
+    { userCode: "QN80", classe: "BTAGO1" },
+    { userCode: "HK47", classe: "BTAGO1" },
+    { userCode: "VT96", classe: "BTAGO1" },
+    { userCode: "JE34", classe: "BTAGO1" },
+    { userCode: "LY52", classe: "BTAGO1" },
+
+    // BTAGO2 (9 élèves)
+    { userCode: "MO71", classe: "BTAGO2" },
+    { userCode: "AZ19", classe: "BTAGO2" },
+    { userCode: "KC83", classe: "BTAGO2" },
+    { userCode: "RF06", classe: "BTAGO2" },
+    { userCode: "PU44", classe: "BTAGO2" },
+    { userCode: "YD90", classe: "BTAGO2" },
+    { userCode: "QH27", classe: "BTAGO2" },
+    { userCode: "NS68", classe: "BTAGO2" },
+    { userCode: "LX11", classe: "BTAGO2" },
+
+    // BTMELEC (5 élèves)
+    { userCode: "CM59", classe: "BTMELEC" },
+    { userCode: "VA14", classe: "BTMELEC" },
+    { userCode: "ZR72", classe: "BTMELEC" },
+    { userCode: "HP03", classe: "BTMELEC" },
+    { userCode: "WX88", classe: "BTMELEC" },
+
+    // C1CAN (8 élèves)
+    { userCode: "JD41", classe: "C1CAN" },
+    { userCode: "EM65", classe: "C1CAN" },
+    { userCode: "TK92", classe: "C1CAN" },
+    { userCode: "QF18", classe: "C1CAN" },
+    { userCode: "LP77", classe: "C1CAN" },
+    { userCode: "ZS24", classe: "C1CAN" },
+    { userCode: "RN50", classe: "C1CAN" },
+    { userCode: "HU09", classe: "C1CAN" },
+
+    // C1HORT (8 élèves)
+    { userCode: "AC36", classe: "C1HORT" },
+    { userCode: "QJ81", classe: "C1HORT" },
+    { userCode: "MS47", classe: "C1HORT" },
+    { userCode: "VR02", classe: "C1HORT" },
+    { userCode: "KX93", classe: "C1HORT" },
+    { userCode: "PD68", classe: "C1HORT" },
+    { userCode: "ET15", classe: "C1HORT" },
+    { userCode: "NB74", classe: "C1HORT" },
+
+    // C1JP (8 élèves)
+    { userCode: "WY59", classe: "C1JP" },
+    { userCode: "HL07", classe: "C1JP" },
+    { userCode: "MX84", classe: "C1JP" },
+    { userCode: "QP22", classe: "C1JP" },
+    { userCode: "SZ96", classe: "C1JP" },
+    { userCode: "RV31", classe: "C1JP" },
+    { userCode: "JC68", classe: "C1JP" },
+    { userCode: "DT10", classe: "C1JP" },
+
+    // C1PSR (9 élèves)
+    { userCode: "KF73", classe: "C1PSR" },
+    { userCode: "PX04", classe: "C1PSR" },
+    { userCode: "NS58", classe: "C1PSR" },
+    { userCode: "QV91", classe: "C1PSR" },
+    { userCode: "HR27", classe: "C1PSR" },
+    { userCode: "LW62", classe: "C1PSR" },
+    { userCode: "ZM19", classe: "C1PSR" },
+    { userCode: "EA80", classe: "C1PSR" },
+    { userCode: "CJ45", classe: "C1PSR" },
+
+    // C1VAN (4 élèves)
+    { userCode: "RH12", classe: "C1VAN" },
+    { userCode: "MV66", classe: "C1VAN" },
+    { userCode: "KP90", classe: "C1VAN" },
+    { userCode: "AZ37", classe: "C1VAN" },
+
+    // C2CAN (8 élèves)
+    { userCode: "TD54", classe: "C2CAN" },
+    { userCode: "QS71", classe: "C2CAN" },
+    { userCode: "LN08", classe: "C2CAN" },
+    { userCode: "PX29", classe: "C2CAN" },
+    { userCode: "RF63", classe: "C2CAN" },
+    { userCode: "WJ16", classe: "C2CAN" },
+    { userCode: "ZH82", classe: "C2CAN" },
+    { userCode: "AC40", classe: "C2CAN" },
+
+    // C2HORT (7 élèves)
+    { userCode: "NE55", classe: "C2HORT" },
+    { userCode: "XQ03", classe: "C2HORT" },
+    { userCode: "HM79", classe: "C2HORT" },
+    { userCode: "VZ24", classe: "C2HORT" },
+    { userCode: "CP68", classe: "C2HORT" },
+    { userCode: "SJ91", classe: "C2HORT" },
+    { userCode: "FT12", classe: "C2HORT" },
+
+    // C2JP (10 élèves)
+    { userCode: "RD27", classe: "C2JP" },
+    { userCode: "WM83", classe: "C2JP" },
+    { userCode: "ZF05", classe: "C2JP" },
+    { userCode: "LC66", classe: "C2JP" },
+    { userCode: "QJ19", classe: "C2JP" },
+    { userCode: "AP74", classe: "C2JP" },
+    { userCode: "SV92", classe: "C2JP" },
+    { userCode: "TK38", classe: "C2JP" },
+    { userCode: "HX11", classe: "C2JP" },
+    { userCode: "GX61", classe: "C2JP" },
+
+    // C2PSR (10 élèves)
+    { userCode: "PM70", classe: "C2PSR" },
+    { userCode: "ZR26", classe: "C2PSR" },
+    { userCode: "NX51", classe: "C2PSR" },
+    { userCode: "CS09", classe: "C2PSR" },
+    { userCode: "VT84", classe: "C2PSR" },
+    { userCode: "KF17", classe: "C2PSR" },
+    { userCode: "QL63", classe: "C2PSR" },
+    { userCode: "MA28", classe: "C2PSR" },
+    { userCode: "RW95", classe: "C2PSR" },
+    { userCode: "DH42", classe: "C2PSR" },
+
+    // C2VAN (6 élèves)
+    { userCode: "ZP60", classe: "C2VAN" },
+    { userCode: "QF14", classe: "C2VAN" },
+    { userCode: "MX88", classe: "C2VAN" },
+    { userCode: "LS23", classe: "C2VAN" },
+    { userCode: "VA71", classe: "C2VAN" },
+    { userCode: "CN05", classe: "C2VAN" }
+];
+
+// Fonction helper pour rechercher un élève par code
+window.trouverEleve = function(userCode) {
+    return window.BDD_ELEVES.find(e => e.userCode === userCode.toUpperCase());
+};
+
+console.log("✅ BDD Élèves chargée :", window.BDD_ELEVES.length, "élèves (RGPD compliant)");
